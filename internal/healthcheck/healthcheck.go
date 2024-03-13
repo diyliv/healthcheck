cc23df306ccf8559aea5b7f2c03d48f804915cb5
diyliv <onlogn081@gmail.com>
2024-03-13 20:14:21 +0300

    just simple struct

--- internal/healthcheck/healthcheck.go
+++ internal/healthcheck/healthcheck.go
@@ -1,1 +1,13 @@
 package healtcheck
+
+import "context"
+
+type healtcheck struct{}
+
+func NewHealthCheck() *healtcheck {
+	return &healtcheck{}
+}
+
+func (h *healtcheck) Init(ctx context.Context) error {
+	return nil
+}
