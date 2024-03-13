6f9d3eae9441cc1d2dcd9e0c73d78328df56aaf3
diyliv <onlogn081@gmail.com>
2024-03-13 20:36:43 +0300

    basic validation

--- internal/opcda/opc_da_healthcheck.go
+++ internal/opcda/opc_da_healthcheck.go
@@ -1,22 +1,27 @@
 package opcda
 
 import (
-	"context"
+	"errors"
 
 	"go.uber.org/zap"
 
+	"github.com/diyliv/opc"
+
 	"github.com/diyliv/healthcheck/internal/models"
+	opcdacheck "github.com/diyliv/healthcheck/pkg/errors"
 )
 
 type healthcheck struct {
 	logger *zap.Logger
+	conn   opc.Connection
 }
 
-func NewHealthCheck(opcda models.OPCDAHealthCheck) *healthcheck {
+func NewHealthCheck(opcda models.OPCDAHealthCheck) (*healthcheck, error) {
+	if opcda.Server == "" {
+		return nil, opcdacheck.ErrNoServerSpecified
+	} else if len(opcda.Nodes) == 0 {
+		return nil, errors.New("node must be specified")
+	}
 
-	return &healthcheck{}
-}
-
-func (h *healthcheck) Init(ctx context.Context) error {
-	return nil
+	return &healthcheck{}, nil
 }
