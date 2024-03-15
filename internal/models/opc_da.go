08f8914a71bad1285e2372b1ac42d96afccf2cf8
diyliv <onlogn081@gmail.com>
2024-03-15 19:15:30 +0300

    added json tags

--- internal/models/opc_da.go
+++ internal/models/opc_da.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type OPCDAHealthCheck struct {
-	Server    string
-	Nodes     []string
-	Tags      []string
-	HeartBeat time.Duration
+	Server    string        `json:"server"`
+	Nodes     []string      `json:"nodes"`
+	Tags      []string      `json:"tags"`
+	HeartBeat time.Duration `json:"heart_beat"`
 }
