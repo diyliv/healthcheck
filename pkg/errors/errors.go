319c5f9109ab3198476a9edcf1151029189e6d1e
diyliv <onlogn081@gmail.com>
2024-03-15 19:17:32 +0300

    added 2 more custom errors

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -5,4 +5,6 @@ import "errors"
 var (
 	ErrNoServerSpecified = errors.New("server must be specified for OPCDA health check")
 	ErrNoNodeSpecified   = errors.New("node must be specified for OPCDA health check")
+	ErrNoOpcConnection   = errors.New("no opc connection was found for provided server")
+	ErrOpcServerExists   = errors.New("provided opc server already exists")
 )
