9dfa86ba2df419d1cb479774c87f830aa3b21e1a
diyliv <onlogn081@gmail.com>
2024-03-15 21:25:00 +0300

    deleted useless variables and decalred new ones declared

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -3,8 +3,9 @@ package errors
 import "errors"
 
 var (
-	ErrNoServerSpecified = errors.New("server must be specified for OPCDA health check")
-	ErrNoNodeSpecified   = errors.New("node must be specified for OPCDA health check")
-	ErrNoOpcConnection   = errors.New("no opc connection was found for provided server")
-	ErrOpcServerExists   = errors.New("provided opc server already exists")
+	ErrAlreadyExistsInCache = errors.New("such value already exists")
+	ErrNoSuchedCachedValue  = errors.New("no such value in cache")
+	ErrOPCDAConvertion      = errors.New("could not convert interface{} to opc.Connection")
+	ErrNoServerSpecified    = errors.New("server must be specified for OPCDA health check")
+	ErrNoNodeSpecified      = errors.New("node must be specified for OPCDA health check")
 )
